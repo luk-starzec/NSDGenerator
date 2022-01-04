a69f10be9623204bb77fa22207e9319dd14d6068
Łukasz Starzec <luk.starzec@gmail.com>
2022-01-04 20:35:29 +0100

    Refactoring

--- NSDGenerator/Client/Services/SerializeService.cs
+++ NSDGenerator/Client/Services/SerializeService.cs
@@ -32,14 +32,14 @@ namespace NSDGenerator.Client.Services
 
         public DiagramModel DeserializeDiagram(string json)
         {
-            var model = JsonSerializer.Deserialize<JsonDiagram>(json, jsonOptions);
+            var jd = JsonSerializer.Deserialize<JsonDiagram>(json, jsonOptions);
 
-            var rootBlock = DeserializeBlocks(model.BlockCollection, model.BlockCollection.RootId);
+            var rootBlock = DeserializeBlocks(jd.BlockCollection, jd.BlockCollection.RootId);
 
             var diagram = new DiagramModel
             {
-                Id = model.Id,
-                Name = model.Name,
+                Id = jd.Id,
+                Name = jd.Name,
                 RootBlock = rootBlock,
             };
 
@@ -52,64 +52,91 @@ namespace NSDGenerator.Client.Services
             if (block is null)
                 return;
 
-            var tb = block as TextBlockModel;
-            if (tb is not null)
-            {
-                var jtb = new JsonTextBlockModel(tb.Id, tb.Text, tb.Child?.Id);
-                result.TextBlocks.Add(jtb);
-
-                SerializeBlocks(tb.Child, result);
+            if (TrySerializeTextBlock(block, result))
                 return;
-            }
+
+            if (TrySerializeBranchBlock(block, result))
+                return;
+        }
+
+        private bool TrySerializeTextBlock(IBlockModel block, JsonBlockCollection jsonBlockCollection)
+        {
+            if (block is not TextBlockModel)
+                return false;
+
+            var tb = block as TextBlockModel;
+            var jtb = new JsonTextBlockModel(tb.Id, tb.Text, tb.Child?.Id);
+            jsonBlockCollection.TextBlocks.Add(jtb);
+
+            SerializeBlocks(tb.Child, jsonBlockCollection);
+            return true;
+        }
+
+        private bool TrySerializeBranchBlock(IBlockModel block, JsonBlockCollection jsonBlockCollection)
+        {
+            if (block is not BranchBlockModel)
+                return false;
 
             var bb = block as BranchBlockModel;
-            if (bb is not null)
-            {
-                var jbb = new JsonBranchBlockModel(bb.Id, bb.Condition, bb.LeftBranch, bb.RightBranch, bb.LeftResult?.Id, bb.RightResult?.Id);
-                result.BranchBlocks.Add(jbb);
+            var jbb = new JsonBranchBlockModel(bb.Id, bb.Condition, bb.LeftBranch, bb.RightBranch, bb.LeftResult?.Id, bb.RightResult?.Id);
+            jsonBlockCollection.BranchBlocks.Add(jbb);
 
-                SerializeBlocks(bb.LeftResult, result);
-                SerializeBlocks(bb.RightResult, result);
-                return;
-            }
+            SerializeBlocks(bb.LeftResult, jsonBlockCollection);
+            SerializeBlocks(bb.RightResult, jsonBlockCollection);
+            return true;
         }
 
         private IBlockModel DeserializeBlocks(JsonBlockCollection blockCollection, Guid currentId)
         {
             var current = blockCollection.Blocks.Where(r => r.Id == currentId).SingleOrDefault();
 
-            var jtb = current as JsonTextBlockModel;
-            if (jtb is not null)
-            {
-                var tb = new TextBlockModel
-                {
-                    Id = jtb.Id,
-                    Text = jtb.Text,
-                };
-                if (jtb.ChildId is not null)
-                    tb.Child = DeserializeBlocks(blockCollection, jtb.ChildId.Value);
-
+            var tb = TryDeserializeTextBlock(current, blockCollection);
+            if (tb is not null)
                 return tb;
-            }
 
-            var jbb = current as JsonBranchBlockModel;
-            if (jbb is not null)
-            {
-                var bb = new BranchBlockModel
-                {
-                    Id = jbb.Id,
-                    Condition = jbb.Condition,
-                    LeftBranch = jbb.LeftBranch,
-                    RightBranch = jbb.RightBranch,
-                };
-                if (jbb.LeftResult is not null)
-                    bb.LeftResult = DeserializeBlocks(blockCollection, jbb.LeftResult.Value);
-                if (jbb.RightResult is not null)
-                    bb.RightResult = DeserializeBlocks(blockCollection, jbb.RightResult.Value);
-
+            var bb = TryDeserializeBranchBlock(current, blockCollection);
+            if(bb is not null)
                 return bb;
-            }
+
             return null;
+        }
+
+        private TextBlockModel TryDeserializeTextBlock(IJsonBlockModel jsonBlock, JsonBlockCollection jsonBlockCollection)
+        {
+            if (jsonBlock is not JsonTextBlockModel)
+                return null;
+
+            var jtb = jsonBlock as JsonTextBlockModel;
+            var tb = new TextBlockModel
+            {
+                Id = jtb.Id,
+                Text = jtb.Text,
+            };
+            if (jtb.ChildId is not null)
+                tb.Child = DeserializeBlocks(jsonBlockCollection, jtb.ChildId.Value);
+
+            return tb;
+        }
+
+        private BranchBlockModel TryDeserializeBranchBlock(IJsonBlockModel jsonBlock, JsonBlockCollection jsonBlockCollection)
+        {
+            if (jsonBlock is not JsonBranchBlockModel)
+                return null;
+
+            var jbb = jsonBlock as JsonBranchBlockModel;
+            var bb = new BranchBlockModel
+            {
+                Id = jbb.Id,
+                Condition = jbb.Condition,
+                LeftBranch = jbb.LeftBranch,
+                RightBranch = jbb.RightBranch,
+            };
+            if (jbb.LeftResult is not null)
+                bb.LeftResult = DeserializeBlocks(jsonBlockCollection, jbb.LeftResult.Value);
+            if (jbb.RightResult is not null)
+                bb.RightResult = DeserializeBlocks(jsonBlockCollection, jbb.RightResult.Value);
+
+            return bb;
         }
 
     }
