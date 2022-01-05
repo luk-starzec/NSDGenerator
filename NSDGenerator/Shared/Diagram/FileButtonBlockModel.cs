ba122b31d7bc2b95c30aec2983f009732f712f62
Łukasz Starzec <luk.starzec@gmail.com>
2022-01-05 21:37:54 +0100

    Dodany komponent FileButtonBlock

---
+++ NSDGenerator/Shared/Diagram/FileButtonBlockModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+
+namespace NSDGenerator.Shared.Diagram;
+
+public class FileButtonBlockModel : IBlockModel
+{
+
+    public IBlockModel Parent { get; set; }
+    public Guid Id { get; set; }
+    public string ButtonText { get; set; }
+    public Action<string> OnFileSelected { get; set; }
+
+    private IBlockModel child;
+    public IBlockModel Child
+    {
+        get => child;
+        set
+        {
+            child = value;
+            if (child is null)
+                child.Parent = this;
+        }
+    }
+
+    public FileButtonBlockModel()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public FileButtonBlockModel(string buttonText, Action<string> onFileSelected) : this()
+        => (ButtonText, OnFileSelected) = (buttonText, onFileSelected);
+
+}
