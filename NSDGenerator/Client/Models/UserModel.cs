806e1ea78ba029e77761b434ebd6be7f056a3a7d
Łukasz Starzec <luk.starzec@gmail.com>
2022-01-10 18:58:47 +0100

    Szkielet strony rejestracji użytkownika

---
+++ NSDGenerator/Client/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSDGenerator.Client.Models;
+
+public class UserModel
+{
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    [MinLength(8, ErrorMessage = "Password should be at least 8 characters long")]
+    public string Password { get; set; }
+
+    [Required, Compare(nameof(Password), ErrorMessage = "Fields Password and Confirm must be the same")]
+    public string PasswordConfirmation { get; set; }
+
+    [Required]
+    public string RegistrationCode { get; set; }
+}
